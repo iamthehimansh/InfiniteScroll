4dd6ba892b15e432b966920e4a994581e7203100
Anyfiddle <hello@anyfiddle.com>
2021-12-23 08:05:38 +0000

    Initial Upload of alfa

---
+++ index.js
@@ -0,0 +1,18 @@
+function InfiniteScroll(ref,OnScroll=()=>{},distance="100px",root=document.body){
+    // 
+    let observer = new IntersectionObserver(async(entries, observer)=>{
+        if(!entries[0].isIntersecting) return
+        await onscroll();
+        observer.unobserve(entries[0].target)
+        observer.observe(ref.current.lastChild)
+        
+    }, {
+        root,
+        rootMargin: distance,
+        threshold: 1.0
+    });
+    observer.observe(ref.current.lastChild)
+}
+
+
+module.exports = InfiniteScroll;
